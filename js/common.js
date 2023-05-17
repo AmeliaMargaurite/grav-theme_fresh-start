e4bb76bc42c948ad13a37f1305ce0a33aaef6b9d
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-05-17 13:49:34 +0200

    new first commit...

---
+++ js/common.js
@@ -0,0 +1,119 @@
+import { throttleFunction } from "./helpers.js";
+const nav = document.getElementById("main-nav");
+const hamburger = document.getElementById("hamburger");
+
+const closeMenuOnEsc = (event) => {
+  if (event.key === "Escape") {
+    closeMenu();
+  }
+};
+
+const closeMenu = () => {
+  hamburger?.classList.remove("open");
+  nav?.classList.remove("open");
+
+  document.body.style.overflowY = "auto";
+  hamburger.setAttribute("aria-expanded", "false");
+
+  window.removeEventListener("keydown", closeMenuOnEsc);
+};
+
+const openMenu = () => {
+  hamburger?.classList.add("open");
+  nav?.classList.add("open");
+
+  document.body.style.overflowY = "auto";
+  window.removeEventListener("keydown", closeMenuOnEsc);
+  hamburger.setAttribute("aria-expanded", "true");
+};
+
+const toggleMenu = (openState = true) => {
+  const isOpen = nav?.classList.contains("open");
+
+  if (isOpen) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+};
+
+const toggleMenuOnWindowSize = (e) => {
+  if (window.innerWidth <= 768) {
+    hamburger.setAttribute("aria-expanded", "false");
+  } else {
+    closeMenu();
+    hamburger.setAttribute("aria-expanded", "true");
+  }
+};
+
+window.addEventListener(
+  "resize",
+  throttleFunction(toggleMenuOnWindowSize, 1500)
+);
+
+window.toggleMenu = toggleMenu;
+
+/***
+ * TYPESCRIPT OPTION
+ ***/
+
+// const closeMenuOnEsc = (event: KeyboardEvent) => {
+// 	if (event.key === "Escape") {
+// 		closeMenu();
+// 	}
+// };
+
+// const closeMenu = () => {
+// 	const hamburger = document.getElementById("hamburger") as HTMLButtonElement;
+// 	const nav = document.getElementById("main-menu");
+
+// 	hamburger?.classList.remove("open");
+// 	nav?.classList.remove("open");
+
+// 	document.body.style.overflowY = "auto";
+// 	hamburger.setAttribute("aria-expanded", "false");
+
+// 	window.removeEventListener("keydown", closeMenuOnEsc);
+// };
+
+// const openMenu = () => {
+// 	const hamburger = document.getElementById("hamburger") as HTMLButtonElement;
+// 	const nav = document.getElementById("main-menu");
+
+// 	hamburger?.classList.add("open");
+// 	nav?.classList.add("open");
+
+// 	document.body.style.overflowY = "auto";
+// 	window.removeEventListener("keydown", closeMenuOnEsc);
+// 	hamburger.setAttribute("aria-expanded", "true");
+// };
+
+// const toggleMenu = (openState = true) => {
+// 	const nav = document.getElementById("main-menu");
+
+// 	const isOpen = nav?.classList.contains("open");
+
+// 	if (isOpen) {
+// 		closeMenu();
+// 	} else {
+// 		openMenu();
+// 	}
+// };
+
+// const toggleMenuOnWindowSize = (e: WindowEventHandlers) => {
+// 	const hamburger = document.getElementById("hamburger") as HTMLButtonElement;
+
+// 	if (window.innerWidth <= 768) {
+// 		hamburger.setAttribute("aria-expanded", "false");
+// 	} else {
+// 		closeMenu();
+// 		hamburger.setAttribute("aria-expanded", "true");
+// 	}
+// };
+
+// window.addEventListener(
+// 	"resize",
+// 	throttleFunction(toggleMenuOnWindowSize, 1500)
+// );
+
+// window.toggleMenu = toggleMenu;
---
+++ js/helpers.js
@@ -0,0 +1,53 @@
+export const throttleFunction = (callBack, delay = 1000) => {
+  let wait = false;
+  let waitingArgs;
+
+  const timeoutFunction = () => {
+    if (waitingArgs == null) {
+      wait = false;
+    } else {
+      callBack(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunction, delay);
+    }
+  };
+
+  return (...args) => {
+    if (wait) {
+      waitingArgs = args;
+      return;
+    }
+
+    callBack(...args);
+    wait = true;
+    setTimeout(timeoutFunction, delay);
+  };
+};
+
+/** TYPESCRIPT OPTION **/
+
+// export const throttleFunction = (callBack: (e?: any) => any, delay = 1000) => {
+//   let wait = false;
+//   let waitingArgs: undefined | any;
+
+//   const timeoutFunction = () => {
+//     if (waitingArgs == null) {
+//       wait = false;
+//     } else {
+//       callBack(...waitingArgs);
+//       waitingArgs = null;
+//       setTimeout(timeoutFunction, delay);
+//     }
+//   };
+
+//   return (...args: any[]) => {
+//     if (wait) {
+//       waitingArgs = args;
+//       return;
+//     }
+
+//     callBack(...args);
+//     wait = true;
+//     setTimeout(timeoutFunction, delay);
+//   };
+// };
